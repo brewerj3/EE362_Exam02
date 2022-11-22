67c8f76eb90204e252ab26e51e2947ed30937f4e
brewerj3 <joshuabrewer784@gmail.com>
2022-11-21 14:32:53 -1000

    updated CMakeLists.txt

--- blocks.c
+++ blocks.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int blocks(int h)
-{
+int blocks(int h) {
     return 0;
 }
 
-void main()
-{
+void main() {
     printf("Number of constructions of blocks of height h\n");
-    for (int h=1; h<=15; h++) {
+    for (int h = 1; h <= 15; h++) {
         printf("h=%2d: %5d\n", h, blocks(h));
     }
 }

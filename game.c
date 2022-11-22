be84b9f60d2894b8069cf1cb9e0c8e90a1137fe9
brewerj3 <joshuabrewer784@gmail.com>
2022-11-22 12:38:21 -1000

    added game.c

--- game.c
+++ game.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 
-int game(int n)
-{
+int game(int n) {
+    //find valid x
+
     return 0;
 }
 
-void main()
-{
-    for (int i=2; i<=12; i++) {
-        printf("game(%2d)=%d\n",i,game(i));
+void main() {
+    for (int i = 2; i <= 12; i++) {
+        printf("game(%2d)=%d\n", i, game(i));
     }
 }

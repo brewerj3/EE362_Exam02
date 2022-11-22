d5465cbdabb3e63480d6660551cd81a2089bd38c
brewerj3 <joshuabrewer784@gmail.com>
2022-11-21 15:14:05 -1000

    updated CMakeLists.txt

--- blocks.c
+++ blocks.c
@@ -2,24 +2,14 @@
 #include <stdio.h>
 
 int binomial(int n, int k) {
-    if(k == 0 || k == n) {
+    if (k == 0 || k == n) {
         return 1;
     }
-    return binomial(n-1, k-1) + binomial(n - 1, k);
+    return binomial(n - 1, k - 1) + binomial(n - 1, k);
 }
 
 int blocks(int h) {
-    int cmOneBlock = binomial(h,1);
-    int twoCmBlock = binomial(h,2);
-    int fourCmBlock = binomial(h+4-1,4);
-    //printf("1cm = %i 2cm = %i 4cm = %i \n",cmOneBlock,twoCmBlock,fourCmBlock);
-    if(h < 2) {
-        return cmOneBlock;
-    }
-    if(h < 4) {
-        return cmOneBlock +twoCmBlock;
-    }
-    return cmOneBlock;
+    return binomial(h,1) * binomial(h,2) * binomial(h, 4);
 }
 
 void main() {

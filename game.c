80250f281a1b954534e0e81f27778aa3ede9fd00
brewerj3 <joshuabrewer784@gmail.com>
2022-11-22 11:56:15 -1000

    added game.c

---
+++ game.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int game(int n)
+{
+    return 0;
+}
+
+void main()
+{
+    for (int i=2; i<=12; i++) {
+        printf("game(%2d)=%d\n",i,game(i));
+    }
+}

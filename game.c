454a299267373248aef43e069836fde137ce4ec5
brewerj3 <joshuabrewer784@gmail.com>
2022-11-22 14:39:20 -1000

    game.c works

--- game.c
+++ game.c
@@ -3,9 +3,19 @@
 
 
 int game(int n) {
-    //find valid x
-
-    return 0;
+    n++; //to simplify the primality test add one to n
+    if (n == 2 || n == 3) {
+        return 1;
+    }
+    if ((n <= 1)|| (n % 2 == 0) || (n % 3 == 0)) {
+        return 0;
+    }
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 void main() {
